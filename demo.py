1d188e30cda964b977c529c2ecc5d6fb4fd8251e
wanted2 <77376569+wanted2@users.noreply.github.com>
2021-05-09 20:38:07 +0700

    Easy game 2: 60FPS in CPU without any cuda

--- demo.py
+++ demo.py
@@ -13,7 +13,10 @@ def parse_args():
     parser.add_argument('--outdir', type=str, default='output/')
     parser.add_argument('--video-path', type=str, default='demo.mp4')
     parser.add_argument('--capture-rate', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=10)
     parser.add_argument('--webcam', action='store_true')
+    parser.add_argument('--parallel', action='store_true')
+    parser.add_argument('--cuda', action='store_true')
     parser.add_argument('--classes', nargs='+')
     return parser.parse_args()
 
@@ -21,18 +24,40 @@ def parse_args():
 def main(args):
     if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
+    # Turn off GPU if needed
+    if not args.cuda:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+        tf.config.set_visible_devices([], 'GPU')
+        visible_devices = tf.config.get_visible_devices()
+        for device in visible_devices:
+            assert device.device_type != 'GPU'
     vpath = 0 if args.webcam else args.video_path
-    predictor = VideoInferenceNoShow(
-        model_path=args.model_path,
-        conf_thres=args.conf_thres,
-        iou_thres=args.iou_thres,
-        target_classes=args.classes
-    )
+    if args.parallel:
+        predictor = VideoInferenceNoShow(
+            model_path=args.model_path,
+            conf_thres=args.conf_thres,
+            iou_thres=args.iou_thres,
+            target_classes=args.classes,
+            batch_size=args.batch_size
+        )
+    else:
+        predictor = VideoInference(
+            model_path=args.model_path,
+            conf_thres=args.conf_thres,
+            iou_thres=args.iou_thres,
+            target_classes=args.classes
+        )
 
     LOGGER.info("Starting the demo ...")
     LOGGER.info("Press 'q' to stop recording")
-    predictor.process_video(
-        vpath, capture_rate=args.capture_rate, outdir=args.outdir)
+    if args.parallel:
+        predictor.process_video(
+            vpath, capture_rate=args.capture_rate, outdir=args.outdir
+        )
+    else:
+        predictor.process_video(
+            vpath, capture_rate=args.capture_rate
+        )
 
 
 if __name__ == '__main__':
